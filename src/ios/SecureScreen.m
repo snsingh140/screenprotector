c7478658083924c90923cb9a95ee1f7fd89d2b79
Shashwat Singh <shashwat@atharvasystem.com>
2022-12-09 15:17:48 +0530

    Confiltion plugin issue resolved

--- src/ios/SecureScreen.m
+++ src/ios/SecureScreen.m
@@ -53,7 +53,7 @@ UITextField* txtField;
 }
 - (void)disable:(CDVInvokedUrlCommand *)command
 {
-    CDVPluginResult* pluginResult = nil;
+    //CDVPluginResult* pluginResult = nil;
     NSLog(@"Disable observers");
    CGRect someRect = [self.webView frame];
     
@@ -62,8 +62,8 @@ UITextField* txtField;
     [self.webView addSubview:txtField];
     //txtField.backgroundColor = [UIColor redColor];
     //[[txtField.centerYAnchor constraintEqualToAnchor:self.webView.centerYAnchor] isActive] = true;
-    [[txtField.centerYAnchor constraintEqualToAnchor:self.webView.centerYAnchor] setActive:YES];
-    [[txtField.centerXAnchor constraintEqualToAnchor:self.webView.centerXAnchor] setActive:YES];
+    [[txtField.centerYAnchor constraintEqualToAnchor:self.webView.centerYAnchor] setActive:NO];
+    [[txtField.centerXAnchor constraintEqualToAnchor:self.webView.centerXAnchor] setActive:NO];
     [self.webView.layer.superlayer addSublayer:txtField.layer];
     [txtField.layer.sublayers.firstObject addSublayer:self.webView.layer];
     txtField.userInteractionEnabled = NO;
